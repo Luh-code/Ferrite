1f73e4a80d386bb15a4d84dfac34556a04c5aa09
luh <Lasse@hueffler.de>
2024-01-07 15:34:59 +0100

    fixed the NodeType Rules

--- src/dom/Rules.java
+++ src/dom/Rules.java
@@ -1,81 +1,83 @@
 package dom;
 
+import java.util.function.Supplier;
+
 // Sub-enum for soc for setting the rule of each NodeType
 // Not sanitized at compile time! Sanitization occurs at runtime whilst tree construction.
 // NodeType.CUSTOM undergoes a post-sanitization
 enum Rules {
-  GENERAL( // TODO: fix this
+  GENERAL(() -> new NodeSettings[]{
           new NodeSettings(NodeType.EXTERNAL),
           new NodeSettings(NodeType.PATH),
           new NodeSettings(NodeType.QUERY)
-  ),
-  SYSTEM(
+  }),
+  SYSTEM(() -> new NodeSettings[]{
           new NodeSettings(NodeType.FERRITE_MAJOR),
           new NodeSettings(NodeType.FERRITE_MINOR)
-  ),
-  FERRITE_MAJOR,
-  FERRITE_MINOR,
-  MAJOR,
-  MINOR,
-  ALIAS,
-  MACHINE(
+  }),
+  FERRITE_MAJOR(() -> new NodeSettings[]{}),
+  FERRITE_MINOR(() -> new NodeSettings[]{}),
+  MAJOR(() -> new NodeSettings[]{}),
+  MINOR(() -> new NodeSettings[]{}),
+  ALIAS(() -> new NodeSettings[]{}),
+  MACHINE(() -> new NodeSettings[]{
           new NodeSettings(NodeType.TRIGGER).setArrayable(),
           new NodeSettings(NodeType.OUTPUT).setArrayable(),
           new NodeSettings(NodeType.STATE).setArrayable()
-  ),
-  EXTERNAL,
-  PATH,
-  STATE(
+  }),
+  EXTERNAL(() -> new NodeSettings[]{}),
+  PATH(() -> new NodeSettings[]{}),
+  STATE(() -> new NodeSettings[]{
           new NodeSettings(NodeType.ORIGIN),
           new NodeSettings(NodeType.ENTRY),
           new NodeSettings(NodeType.BEGIN),
           new NodeSettings(NodeType.TRANSITION).setArrayable()
-  ),
-  TRIGGER(
+  }),
+  TRIGGER(() -> new NodeSettings[]{
           new NodeSettings(NodeType.TYPE),
           new NodeSettings(NodeType.CUSTOM).setArrayable()
-  ),
-  OUTPUT(
+  }),
+  OUTPUT(() -> new NodeSettings[]{
           new NodeSettings(NodeType.TYPE),
           new NodeSettings(NodeType.CUSTOM).setArrayable()
-  ),
-  ORIGIN,
-  ENTRY,
-  BEGIN,
-  TRANSITION(
+  }),
+  ORIGIN(() -> new NodeSettings[]{}),
+  ENTRY(() -> new NodeSettings[]{}),
+  BEGIN(() -> new NodeSettings[]{}),
+  TRANSITION(() -> new NodeSettings[]{
           new NodeSettings(NodeType.STATE),
           new NodeSettings(NodeType.IF).setArrayable()
-  ),
-  TYPE,
-  IF(
+  }),
+  TYPE(() -> new NodeSettings[]{}),
+  IF(() -> new NodeSettings[]{
           new NodeSettings(NodeType.EQUALS).setArrayable(),
           new NodeSettings(NodeType.LESSER).setArrayable(),
           new NodeSettings(NodeType.GREATER).setArrayable()
-  ),
-  EQUALS(
+  }),
+  EQUALS(() -> new NodeSettings[]{
           new NodeSettings(NodeType.TRIGGER),
           new NodeSettings(NodeType.VALUE)
-  ),
-  LESSER(
+  }),
+  LESSER(() -> new NodeSettings[]{
           new NodeSettings(NodeType.TRIGGER),
           new NodeSettings(NodeType.VALUE)
-  ),
-  GREATER(
+  }),
+  GREATER(() -> new NodeSettings[]{
           new NodeSettings(NodeType.TRIGGER),
           new NodeSettings(NodeType.VALUE)
-  ),
-  VALUE,
-  QUERY,
-  CUSTOM; // has to always remain empty here, as rules for a custom node are handled later down the line
+  }),
+  VALUE(() -> new NodeSettings[]{}),
+  QUERY(() -> new NodeSettings[]{}),
+  CUSTOM(() -> new NodeSettings[]{}); // has to always remain empty here, as rules for a custom node are handled later down the line
 
-  private NodeSettings[] settings;
+  private Supplier<NodeSettings[]> settings;
 
-  Rules(NodeSettings... settings) {
+  Rules(Supplier<NodeSettings[]> settings) {
     this.settings = settings;
     //this.rules = Arrays.stream(this.rules).toList().addAll(Rules.GENERAL.getRules());
   }
 
   public NodeSettings[] getSettings() {
-    return this.settings;
+    return this.settings.get();
   }
 }
